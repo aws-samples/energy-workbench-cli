56854ca2abcdd329ce269f8f0cb9048796c444a3
wfrazao <wfrazao@amazon.com>
2023-10-19 19:40:55 -0500

    create group api

---
+++ src/commands/group/add.ts
@@ -0,0 +1,27 @@
+import {Args, Command, Flags} from '@oclif/core'
+import { GROUP } from "osdu-workbench-sdk";
+import {AddGroup} from "osdu-workbench-sdk/dist/group/group.models";
+
+export default class GroupAdd extends Command {
+  static description = 'Adds a group'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+  ]
+
+  static args = {
+    groupToAdd: Args.string({ description: "Group to add" }),
+  };
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(GroupAdd)
+    const baseURL = "https://osdu.osdupsdemo.install.osdu.aws";
+    const group = new GROUP.GroupAdd(baseURL, "us-east-1");
+
+    const g = args.groupToAdd || "";
+
+    const response = await group.add(g);
+
+    console.log(JSON.stringify(response, null, 2))
+
+  }
+}
--- src/commands/group/list.ts
+++ src/commands/group/list.ts
@@ -1,5 +1,5 @@
 import {Args, Command, Flags} from '@oclif/core'
-import { Group } from "osdu-workbench-sdk";
+import { GROUP } from "osdu-workbench-sdk";
 import { displayGroupResults } from "../../utils/group/groupListTable";
 
 export default class GroupSearch extends Command {
@@ -12,7 +12,7 @@ export default class GroupSearch extends Command {
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(GroupSearch)
     const baseURL = "https://osdu.osdupsdemo.install.osdu.aws";
-    const group = new Group.GroupClient(baseURL, "us-east-1");
+    const group = new GROUP.GroupList(baseURL, "us-east-1");
     const response = await group.query();
 
     displayGroupResults(response);
