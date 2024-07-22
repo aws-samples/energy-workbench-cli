ea5299af01071188833185e978d3dd7f23ae4991
wfrazao <wfrazao@amazon.com>
2024-07-22 16:16:35 -0500

    fixing session id for bedrock agents

--- src/commands/qs.ts
+++ src/commands/qs.ts
@@ -1,4 +1,4 @@
-import { Args, Command } from '@oclif/core'
+import { Args, Command, Flags } from '@oclif/core'
 import { validateEnv } from "../utils/config/config.js";
 import {
   BedrockAgentRuntimeClient,
@@ -9,21 +9,28 @@ import { fromIni } from "@aws-sdk/credential-providers";
 import { join } from "path";
 
 export default class QSearch extends Command {
-  static override description = 'Sends a natural language search to Energy Data Insights search agent.'
-  static override examples = [
-    '<%= config.bin %> <%= command.id %> what are the coordinates for well BRK-06',
+  static description = 'Sends a natural language search query to Energy Data Insights search agent.'
+  static examples = [
+    '<%= config.bin %> <%= command.id %> "what are the coordinates for well BRK-06" --session=test01',
   ]
 
-  static override args = {
+  static args = {
     qPrompt: Args.string({
       required: true,
       description: 'Prompt to send to the agent.'
     }),
   }
 
-  public async run(): Promise<void> {
-    const { args } = await this.parse(QSearch)
+  static flags = {
+    session: Flags.string({
+      description: 'Specifies the session ID for the agent interaction.',
+      required: false,
+    }),
+  };
 
+  public async run(): Promise<void> {
+    const { args, flags } = await this.parse(QSearch);
+    const sessionId = flags.session || "default_session_id";
     const config = validateEnv();
     const profileName = config.aws_profile;
 
@@ -38,7 +45,6 @@ export default class QSearch extends Command {
 
     const agentId = "OWALQZA4WC";
     const agentAliasId = "OI9NY9FWNU";
-    const sessionId = "edisearch";
 
     const command = new InvokeAgentCommand({
       agentId,
